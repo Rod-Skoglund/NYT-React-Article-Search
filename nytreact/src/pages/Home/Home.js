d9cd6b8dd6e18cae7c576fed3e92de34df318583
RLS <rod.skoglund@gmail.com>
2018-10-03 02:39:50 -0500

    added copy of package.json to parent level

---
+++ nytreact/src/pages/Home/Home.js
@@ -0,0 +1,73 @@
+// ******************************************************************************
+// Program: NYT React Article Search
+// Authors: Rod Skoglund
+// File: (src-pages-Home) Home.js
+// Description: Creates the Home Page React Component.
+// ******************************************************************************
+
+// ******************************************************************************
+// Imports React Component & compnents/utilities
+// ******************************************************************************
+import React, {Component} from "react";
+
+import SearchForm from "../../components/SearchForm";
+import Results from "../../components/Results";
+import API from "../../utils/API";
+import Nav from '../../components/NavBar';
+
+// ******************************************************************************
+// Define a new React Class Component. 
+// ******************************************************************************
+class Home extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      articles : [],
+      limit: null
+    }; // End this.state
+
+    this.newQuery = this.newQuery.bind(this);
+    this.searchArticles = this.searchArticles.bind(this);
+  } // End constructor
+
+
+  newQuery({searchParams}){
+    let {topic, startDate, endDate, limit} = searchParams;
+    this.setState({limit}); // Set the limit of Articles to Show
+    let queryString = `${topic}${startDate}${endDate}`;
+    this.searchArticles(queryString);
+  } // End newQuery
+
+  searchArticles = query => {
+    API.search(query).then(res => {
+    let articlesArray = [];
+
+    res.data.response.docs.map(({snippet, web_url, pub_date, _id, multimedia}) => {
+        articlesArray.push({title: snippet, url: web_url, date: pub_date, articleId: _id , image: multimedia[2]});
+      });
+
+
+    this.setState(prevState => ({
+      articles: [...prevState].concat(articlesArray).splice(0, this.state.limit)
+    }), console.log(this.state.limit))
+
+  }).catch(err => console.log(err));
+}; // End searchArticles
+
+  render() {
+    // console.log("New is " + JSON.stringify(this.state))
+    return (
+    <React.Fragment>
+      <Nav/>
+        <div className = "container">
+          <SearchForm newQuery={this.newQuery}/>
+          <Results results={this.state}/>
+        </div>
+    </React.Fragment>
+    ) // End return
+  } // End render
+
+} // End class
+
+export default Home;
